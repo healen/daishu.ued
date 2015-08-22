575cf3b1b8709bf1f0742bf5c4d60acc4b8a78f9
zhangxiaodong <zhangxiaodong@daishu360.com>
2015-08-22 17:48:25 +0800

    新增债权转让，

--- mObjects/m.daishu.com.defalut/static/js/form.js
+++ mObjects/m.daishu.com.defalut/static/js/form.js
@@ -30,6 +30,31 @@
 			}
 		})
 	}
+
+	$.yanzheng=function(element,times,callBackStart,callBackEnd){
+		var sves=times;
+		var timeout=null;
+		$(element).live("click",function(){
+			callBackStart && callBackStart();
+			if($(this).attr("disabled")!="disabled"){
+				var that=$(this);
+				clearInterval(timeout);
+				that.attr("disabled", "disabled");
+				timeout=setInterval(function(){
+					that.val(times + "秒再次发送");
+	                times--;
+	                if (times < 0) {
+	                    clearInterval(timeout);
+	                    that.val("发送验证码");
+	                    times = sves;
+	                    that.removeAttr("disabled");
+	                    callBackEnd && callBackEnd();
+	                }
+				},1000)
+
+			}
+		})
+	}
 	
 
 
